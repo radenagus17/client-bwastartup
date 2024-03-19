758a9fbd83fdb487e72f1774704164f8acf86f14
radenagus17_ <vaghuz@gmail.com>
2024-03-19 14:53:46 +0700

    :tada: Initial Project: add page home &  page error

---
+++ error.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <div class="container mx-auto h-screen flex justify-center items-center">
+    <div class="w-full lg:w-1/3 px-10 lg:px-0">
+      <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+        <img src="/assets/images/404-illustration.svg" alt="" class="w-full" />
+      </div>
+      <h2 class="font-medium mb-3 text-3xl text-center">
+        Oops! Something went wrong.
+      </h2>
+      <p class="text-center font-light" v-if="error?.statusCode === 404">
+        The page that you requested doesn't
+        <br />
+        exist at this moment
+      </p>
+      <p class="text-center font-light" v-else>An error occurred</p>
+      <div class="mb-4 mt-6">
+        <div class="mb-3">
+          <button
+            @click="handleError"
+            class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
+          >
+            Back To Home
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Poppins: [200, 300, 400, 500, 600, 700, 800],
+    },
+  },
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
